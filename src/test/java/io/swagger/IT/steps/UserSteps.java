ec09e1eb19a7009cae54d895897253a601882232
rster20002 <rster2002dev@gmail.com>
2021-06-05 13:01:00 +0200

    🚚 Moved dummy data from TestConsole to StartupApplicationRunner

--- src/test/java/io/swagger/IT/steps/UserSteps.java
+++ src/test/java/io/swagger/IT/steps/UserSteps.java
@@ -4,18 +4,24 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.swagger.model.User;
 import io.swagger.model.dto.ExceptionDTO;
 import io.swagger.model.dto.LoginDTO;
 import io.swagger.model.dto.LoginResponseDTO;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Objects;
 
 public class UserSteps {
@@ -33,17 +39,12 @@ public class UserSteps {
         loginDTO.setEmailAddress(emailAddress);
         loginDTO.setPassword(password);
 
-        try {
-            String requestBody = objectMapper.writeValueAsString(loginDTO);
+        String requestBody = objectMapper.writeValueAsString(loginDTO);
 
-            HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
-            ResponseEntity<LoginResponseDTO> responseDTO = restTemplate.postForEntity(uri, entity, LoginResponseDTO.class);
+        HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+        ResponseEntity<LoginResponseDTO> responseDTO = restTemplate.postForEntity(uri, entity, LoginResponseDTO.class);
 
-            return Objects.requireNonNull(responseDTO.getBody()).getAuthToken();
-        } catch (Exception e) {
-            System.out.println(e);
-            return null;
-        }
+        return "Bearer " + Objects.requireNonNull(responseDTO.getBody()).getAuthToken();
     }
 
     @When("I make a request to the /users API endpoint without an authentication token")
@@ -61,6 +62,8 @@ public class UserSteps {
         Assert.assertEquals(expectedHttpStatusCode, httpClientErrorException.getRawStatusCode());
     }
 
+    private ResponseEntity<String> stringResponse;
+
     @When("An employee makes a request to the \\/users API endpoint")
     public void anEmployeeMakesARequestToTheUsersAPIEndpoint() throws URISyntaxException, JsonProcessingException {
         URI uri = new URI(baseUrl + "/users");
@@ -68,6 +71,13 @@ public class UserSteps {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", getJwtToken("alice@example.com", "idk"));
 
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        stringResponse = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+    }
 
+    @Then("The server will return list of {int} users")
+    public void theServerWillReturnListOfUsers(int arg0) throws JSONException {
+        JSONArray jsonObject = new JSONArray(stringResponse.getBody());
+        Assert.assertEquals(arg0, jsonObject.length());
     }
 }
--- src/test/resources/features/users.feature
+++ src/test/resources/features/users.feature
@@ -5,4 +5,4 @@ Feature: User tests
 
   Scenario: Get all users from the API performed by employee
     When An employee makes a request to the /users API endpoint
-    Then The server will return list of users
+    Then The server will return list of 3 users
